7c4d77ae04de9ea3dcea19e254a1d51bbf0f7124
bulbul-016 <b_kasymbek@kbtu.kz>
2021-06-27 08:35:08 +0600

    '27'

---
+++ 2/1.py
@@ -0,0 +1,12 @@
+
+'''
+L2. Python collections.
+1. Python Lists
+2. Python Tuples
+3. Python Sets
+4. Python Dictionaries
+5. Python Functions
+6. Python Lambda
+7. Python Classes and Objects.
+TSIS 2
+8. Python Inheritance'''
